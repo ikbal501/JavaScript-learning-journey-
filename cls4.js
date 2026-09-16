f818bc6b7bc152c419494e6d8f8895f12bd4c059
IKBAL AHMED <ikbalahmed1231@gmail.com>
2026-09-16 12:58:32 +0530

    more feature added

--- cls4.js
+++ cls4.js
@@ -4,4 +4,9 @@ import { stdin as input, stdout as output } from "process";
 const rl = readline.createInterface({ input, output });
 
 let num = Number(await rl.question("enter any number: "));
-let num2 = Number(await rl.question("enter any number"));
+let num2 = Number(await rl.question("enter any number: "));
+
+console.log(num);
+console.log(num2);
+
+rl.close();
